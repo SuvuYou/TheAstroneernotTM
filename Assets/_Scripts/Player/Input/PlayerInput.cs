6b0ced4bbfe19ff759bb2ce537e541be50555908
SuvuYou <nazarsuvyliak1@gmail.com>
2024-12-13 20:36:12 +0200

    add player inputs

--- Assets/_Scripts/Player/Input/PlayerInput.cs
+++ Assets/_Scripts/Player/Input/PlayerInput.cs
@@ -8,10 +8,17 @@ class PlayerInput : MonoBehaviour
     private void Update()
     {
         _collectMouseMovementInput();
+        _collectMovementInput();
+
+        _collectIsSpaceDownInput();
+
         _collectMouseButtonInput();
     }
 
     private void _collectMouseMovementInput() => _playerInputValues.SetMouseMovementInput(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+    private void _collectMovementInput() => _playerInputValues.SetMovementInput(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+
+    private void _collectIsSpaceDownInput() => _playerInputValues.SetIsSpaceDown(Input.GetKeyDown(KeyCode.Space));
 
     private void _collectMouseButtonInput()
     {
--- Assets/_Scripts/Player/Input/PlayerInputValues.cs
+++ Assets/_Scripts/Player/Input/PlayerInputValues.cs
@@ -6,11 +6,15 @@ class PlayerInputValues : ScriptableObject
     public Vector2 MouseMovementInput { get; private set; }
     public Vector2 MovementInput { get; private set; }
 
+    public bool IsSpaceDown { get; private set; }
+
     public bool IsHoldingRightMouseButton { get; private set; }
     public bool IsHoldingLeftMouseButton { get; private set; }
 
     public void SetMouseMovementInput(Vector2 mouseMovementInput) => MouseMovementInput = mouseMovementInput;
     public void SetMovementInput(Vector2 movementInput) => MovementInput = movementInput;
+
+    public void SetIsSpaceDown(bool isSpaceDown) => IsSpaceDown = isSpaceDown;
 
     public void SetIsHoldingRightMouseButton(bool isHoldingRightMouseButton) => IsHoldingRightMouseButton = isHoldingRightMouseButton;
     public void SetIsHoldingLeftMouseButton(bool isHoldingLeftMouseButton) => IsHoldingLeftMouseButton = isHoldingLeftMouseButton;
